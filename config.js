be37d5cd2a188a2341888941c90955b48332c76b
sader <sader@jrs>
2025-10-04 15:44:39 +0200

    update git ignore

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export const PROD =false;
 
 export const FRONT_URI = "http://localhost:3000";
 export const API_PORT = 5000;
-export const DB_NAME= "notes_db.db";
+export const DB_NAME= "n_db.db";
 
 
 
